53bb4655356c6bb9ef9be34a0be14583e32486a1
Gedeilson Lopes <gedeilsonlps@gmail.com>
2021-07-21 20:36:26 -0300

    feat: Adicionando nome

--- exercicio1.ts
+++ exercicio1.ts
@@ -1,15 +1,4 @@
 // Adicionar o seu nome aqui no vetor, na mesma linha
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const alunos = ['Ricardo de Jesus'];
-=======
-export const alunos = ["Leonardo de Quadros Gonçalves"];
->>>>>>> f1606ff110db3f469e4fef5da0b0159e7b10bb7a
-=======
-export const alunos = [
-    "Leonardo de Quadros Gonçalves",
-    "Henrique Caetano de Barros",
-    "Alex Botelho de Almeida",
-    "Henrique Kenji Nawa"
-];
->>>>>>> d631ca62746130b243270b90f6f14d27310fcd6f
+export const alunos = ["Gedeilson Lopes"];
+
+
--- exercicio2.ts
+++ exercicio2.ts
@@ -1,12 +1,4 @@
 // Adicionar o seu nome aqui no vetor, em outra linha
 export const alunos = [
-
-
-    'Ricardo de Jesus',"Mendes","Mendes",
-
-   "Henrique",
-
-   "Henrique",
-   "Henrique Kenji Nawa"
-
+  'Gedeilson Lopes'
 ];
