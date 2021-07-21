1e5c1fbe8fa5817d802ed872f961e48a123fc7f4
hcbarros <48363350+hcbarros@users.noreply.github.com>
2021-07-21 20:17:25 -0300

    Update exercicio1.ts

--- exercicio1.ts
+++ exercicio1.ts
@@ -1,2 +1,2 @@
 // Adicionar o seu nome aqui no vetor, na mesma linha
-export const alunos = ["Leonardo de Quadros Gonçalves"];
+export const alunos = ["Leonardo de Quadros Gonçalves","Henrique Caetano de Barros"];
