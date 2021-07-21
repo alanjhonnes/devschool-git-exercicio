6f35be19805e4c0075551fe9fcdb11e5f84cc098
ricardo301 <ricardo9251@gmail.com>
2021-07-21 20:24:50 -0300

    Ricardo

--- exercicio2.ts
+++ exercicio2.ts
@@ -1,16 +1,12 @@
 // Adicionar o seu nome aqui no vetor, em outra linha
 export const alunos = [
-<<<<<<< HEAD
 
-    'Ricardo de Jesus',"Mendes"
 
-=======
-   "Mendes",
-<<<<<<< HEAD
-   "Henrique"
->>>>>>> d631ca62746130b243270b90f6f14d27310fcd6f
-=======
+    'Ricardo de Jesus',"Mendes","Mendes",
+
+   "Henrique",
+
    "Henrique",
    "Henrique Kenji Nawa"
->>>>>>> 5a8326e8121f05d915c00413d2235620e9624396
+
 ];
